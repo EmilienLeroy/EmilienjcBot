c6345925286dab56a4935c58601e990ff89566bc
Emilien Leroy <emilien1005@hotmail.fr>
2021-09-27 21:21:25 +0100

    🎉 (robot): create robot package

---
+++ packages/robot/README.md
@@ -0,0 +1,11 @@
+# `robot`
+
+> TODO: description
+
+## Usage
+
+```
+const bot = require('robot');
+
+// TODO: DEMONSTRATE API
+```
---
+++ packages/robot/src/index.ts
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const {} = process.env;
+
+async function bootstrap() {
+    try {
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+bootstrap();
