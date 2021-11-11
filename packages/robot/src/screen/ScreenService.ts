1522b47bf106cf39db9949b2e02f337ca3de318f
Emilien Leroy <emilien1005@hotmail.fr>
2021-11-11 17:07:35 +0000

    ✨ (robot): Display command on screen when this is consumme

--- packages/robot/src/screen/ScreenService.ts
+++ packages/robot/src/screen/ScreenService.ts
@@ -1,4 +1,4 @@
-import { CommandManager } from "@emilienjc/command";
+import { CommandManager, QueuedCommand } from "@emilienjc/command";
 import { IPublishPacket } from "mqtt";
 import { container, inject, singleton } from "tsyringe";
 import { Display } from "./display";
@@ -13,9 +13,23 @@ export class ScreenService {
     try {
       this.displays = container.resolveAll('display');
       this.displays.forEach((display) => display.displayReady());
+      commandManager.on('consumeQueue', this.displayCommand.bind(this));
     } catch (error) {
       console.warn('No display configured, you won\'t be able to see the robot state !')
     }
+  }
+
+  public displayCommand(command: QueuedCommand<Buffer, IPublishPacket>) {
+    const { subscriber } = command.params
+    let message = '';
+
+    try {
+      message = JSON.parse(command.params.payload.toString()).action;  
+    } catch (error) {
+      message = command.params.payload.toString();
+    }
+    
+    this.displays?.forEach((d) => d.displayCommand(`${subscriber} ${message}`));
   }
 
 }
